a2e8cb13a33fece7a7fff4c2fb66fbf65eb070cf
yuchu <m45j6huangyuchu@gmail.com>
2016-03-20 10:25:26 -0400

    update

--- FunctionsBlockIQ.js
+++ FunctionsBlockIQ.js
@@ -17,24 +17,37 @@ var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/ton
 
 
 //Search Function
-new L.Control.GeoSearch({
-    provider: new L.GeoSearch.Provider.Google(),
-    position: 'topcenter',
-    showMarker: true,
-    retainZoomLevel: true
-}).addTo(map);
-
 var censusCall = "long lat empty";
 var tractCall = "tract empty";
 var FIPSCode = "No FIPS Code";
 
 var processFIPSCode = function(FIPSCode) {
-  console.log(FIPSCode);
+  // console.log(FIPSCode);
   var state = FIPSCode.substring(0,2);
   var county = FIPSCode.substring(2,5);
   var tract = FIPSCode.substring(5,11);
   var block = FIPSCode.substring(11,15);
   FIPStoinfo(state,county,tract);
+};
+
+var CensusTract;
+
+var removeCensusTract = function(CensusTract) {
+      map.removeLayer(CensusTract);
+    };
+
+var CensusTractGeoJSON = function(FIPSCode){
+  url = "https://raw.githubusercontent.com/yuchu/BlockIQ/master/Census_Tracts_2010.geojson";
+  $.ajax(url).done(function(data){
+    parsed= JSON.parse(data);
+    console.log(parsed);
+    CensusTract = L.geoJson(parsed, {
+      filter: function(feature){
+        return (feature.properties.GEOID10==FIPSCode.substring(0,11));
+      }
+    }).addTo(map);
+    // console.log(FIPSCode,FIPSCode.substring(0,11));
+  });
 };
 
 var FIPStoinfo = function(state,county,tract){
@@ -43,23 +56,39 @@ var FIPStoinfo = function(state,county,tract){
   params = "?get=B25119_001E&"; //Test parameters
   geography = "for=tract:" + tract + "&in=state:" + state+ "+county:" + county;
   tractCall = httpCensus + params + geography + censusKey;
-  console.log(tractCall);
+  // console.log(tractCall);
   $.ajax({
     url: tractCall,
     crossDomain:true,
     success: function (data, textStatus, xhr) {
-      console.log(data);
+      // console.log(data);
   }
 });
 };
 
-
 //Get Long and Lat from center of map, then call FCC Block Converter API
 //On pressing enter in search bar
-$("#leaflet-control-geosearch-qry").keypress(function(e) {
-    if(e.which == 13) {
-    var latitude = map.getCenter().lat;
-    var longitude = map.getCenter().lng;
+
+var marker = [];
+var plotMarkers = function(marker){
+      marker.addTo(map);
+    };
+var removeMarker = function(marker) {
+      map.removeLayer(marker);
+    };
+
+var geocoder = function() {
+  var query = $('#addressInput').val();
+
+  $.ajax({
+    url: "http://nominatim.openstreetmap.org/search?format=json&addressdetails=0&limit=1&countrycodes=US&q="+query+" philadelphia pa",
+  }).done(function(data) {
+    console.log(data, _.first(data).lat, _.first(data).lon);
+    var latitude = _.first(data).lat;
+    var longitude = _.first(data).lon;
+    marker = L.marker([latitude, longitude]);
+    plotMarkers(marker);
+
     censusCall = "https://data.fcc.gov/api/block/find?format=jsonp&latitude=" + latitude +"&longitude="+ longitude + "&showall=false";
     $.ajax({
       url: censusCall,
@@ -68,13 +97,46 @@ $("#leaflet-control-geosearch-qry").keypress(function(e) {
       crossDomain:true,
       success: function (data, textStatus, xhr) {
       FIPSCode = data.Block.FIPS;
+      console.log(FIPSCode);
       processFIPSCode(FIPSCode);
+      CensusTractGeoJSON(FIPSCode);
+      // plotCensusTract();
+      if(typeof CensusTract !== 'undefined'){
+        removeCensusTract(CensusTract);
+      }
     }
   });
-}});
+  });
+};
+
+$('#addressInput').keypress(function(e){
+  removeMarker(marker);
+
+  if(e.which == 13) {
+    geocoder();
+
+  }
+});
 
 
-
+// $("#leaflet-control-geosearch-qry").keypress(function(e) {
+//     if(e.which == 13) {
+//     var latitude = control._positionMarker._latlng.lat;
+//     var longitude = control._positionMarker._latlng.lng;
+//     censusCall = "https://data.fcc.gov/api/block/find?format=jsonp&latitude=" + latitude +"&longitude="+ longitude + "&showall=false";
+//     $.ajax({
+//       url: censusCall,
+//       type: 'GET',
+//       dataType: 'jsonp',
+//       crossDomain:true,
+//       success: function (data, textStatus, xhr) {
+//       FIPSCode = data.Block.FIPS;
+//       console.log(FIPSCode);
+//       processFIPSCode(FIPSCode);
+//       drawCensusTract(FIPSCode);
+//     }
+//   });
+// }});
 
 //Load first page
 $(document).ready(function(){
